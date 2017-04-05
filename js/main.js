5ee852f22db1bb40af07dd43884308cc5f56bd5b
Chor Quiambao <chorquiambao@gmail.com>
2017-04-05 19:40:13 +0800

    creatives
    Updated creatives added modal

--- js/main.js
+++ js/main.js
@@ -1,25 +1,25 @@
-(function($){
-	$(window).load(function(){
-		
-        /*	FlexSlide text
+(function ($) {
+    $(window).load(function () {
+
+        /*  FlexSlide text
         /*----------------------------------------------------*/
-		$('.flexslider').flexslider({
-			animation: "slide",
-			selector: ".flex-slogan > li",
-			controlNav: false,
-			directionNav: false,
-			direction: "vertical",
-			slideshowSpeed: 4000,
-			keyboard: true,
-			touch: false,
-         });
-        
-		
-        /*	countdown
+        $('.flexslider').flexslider({
+            animation: "slide",
+            selector: ".flex-slogan > li",
+            controlNav: false,
+            directionNav: false,
+            direction: "vertical",
+            slideshowSpeed: 4000,
+            keyboard: true,
+            touch: false,
+        });
+
+
+        /*  countdown
         /*----------------------------------------------------*/
         var dateFinal = '2019/11/01';
 
-        $('.countdown').countdown(dateFinal, function(event) {
+        $('.countdown').countdown(dateFinal, function (event) {
 
             var $this = $(this)
 
@@ -29,23 +29,49 @@
             $this.find('span.minutes').html(event.strftime('%M'));
             $this.find('span.seconds').html(event.strftime('%S'));
         });
-        
-	});
-    
-    /*	slideshow1 ( nivo Slider )
-    /*----------------------------------------------------*/
-    $(function(){
+        /*  modal
+        /*----------------------------------------------------*/
+        function iframeModalOpen() {
 
-            startSlideshow();
-
-        })
-
-        function startSlideshow() {
-
-            $('#nivoSlider').nivoSlider({
-                effect: 'slideInRight'
+            $('.modalButton').on('click', function (e) {
+                var src = $(this).attr('data-src');
+                var width = $(this).attr('data-width');
+                var height = $(this).attr('data-height');
+                $(".modal-dialog").width(width);
+                $(".modal-dialog").height(height);
+                var allowfullscreen = $(this).attr('data-video-fullscreen');
+                $("#myModal iframe").attr({
+                    'src': src,
+                    'allowfullscreen': ''
+                });
             });
 
+            $('#myModal').on('hidden.bs.modal', function () {
+                $(this).find('iframe').html("");
+                $(this).find('iframe').attr("src", "");
+            });
         }
-    
+
+        $(document).ready(function () {
+            iframeModalOpen();
+        });
+
+    });
+
+    /*  slideshow1 ( nivo Slider )
+    /*----------------------------------------------------*/
+    $(function () {
+
+        startSlideshow();
+
+    })
+
+    function startSlideshow() {
+
+        $('#nivoSlider').nivoSlider({
+            effect: 'slideInRight'
+        });
+
+    }
+
 })(jQuery);
